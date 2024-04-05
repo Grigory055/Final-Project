6172acd5d075b43c43cf1785424fcabd262583c5
ko-tenka <kotelnikova123.123@gmail.com>
2024-04-05 20:07:31 +0700

    release mar

--- client/src/components/MainMenu/MainMenu.tsx
+++ client/src/components/MainMenu/MainMenu.tsx
@@ -1,13 +1,28 @@
 import { Button, Box, Typography } from "@mui/material";
-import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ReactRouterLink,  useNavigate  } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { fetchUserLogout } from "../../redux/thunkActions";
 
 export function MainMenu() {
+  const navigate = useNavigate();
+  const isLogin = useAppSelector((store) => store.isLogin);
+
+  const dispatch = useAppDispatch();
+
+  const logoutHandler = async (e: React.FormEvent): Promise<void> => {
+    e.preventDefault()
+    if (isLogin) {
+      void dispatch(fetchUserLogout());
+      navigate('/login');
+    }
+  };
   return (
     <Box display="flex" flexDirection="column" gap={2}>
       <Typography variant="h4" id="MainMenu_gl" textAlign="center">Главное меню</Typography>
       <Button component={ReactRouterLink} to="/newgame" variant="contained" id="MainMenu_p" type="submit" size="large">Новая Игра</Button>
       <Button component={ReactRouterLink} to="/stats"variant="contained" id="MainMenu_p" type="submit" size="large">Статистика</Button>
-      <Button component={ReactRouterLink} to="/logout"variant="contained" id="MainMenu_p" type="submit" size="large">Выйти</Button>
+      <Button onClick={logoutHandler}  variant="contained" id="MainMenu_p" type="submit" size="large">Выйти</Button>
+
     </Box>
   )
 }
