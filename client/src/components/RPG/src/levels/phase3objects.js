e91662012e4e5a2e74c64635f029afd0addd4998
Grigory055 <154119543+Grigory055@users.noreply.github.com>
2024-04-14 21:21:24 +0300

    posPh3

--- client/src/components/RPG/src/levels/phase3objects.js
+++ client/src/components/RPG/src/levels/phase3objects.js
@@ -1,29 +1,29 @@
 export const phase3objects = {
     hero: {
       position: {
-        x: 19, // 19
-        y: 53, // 53
+        x: 68, // 19
+        y: 19, // 53
       },
     },
     rod1: {
-      x: 40, // 40
-      y: 44, // 44
+      x: 49, // 40 //fio
+      y: 45, // 44
     },
     rod2: {
-      x: 56, // 56
-      y: 19, // 19
+      x: 24, // 56
+      y: 35, // 19
     },
     rod3: {
-      x: 25, // 25
-      y: 30, // 30
+      x: 49, // 25
+      y: 33, // 30
     },
-    dialogBubble: {
-      x: 26,
-      y: 41,
+    dialogBubble: { // modal
+      x: 54,
+      y: 51,
     },
-    npc1: {
-      x: 46, // 46
-      y: 40, // 40
+    npc1: { // svet
+      x: 1, // 46
+      y: 1, // 40
       exit: {
         x: 0,
         y: 0,
@@ -31,8 +31,8 @@ export const phase3objects = {
       skin: 0,
     },
     npc2: {
-      x: 23, // 23
-      y: 28, // 28
+      x: 30, // 23
+      y: 24, // 28
       exit: {
         x: 0,
         y: 0,
