d7ad10ad89fc06cd3b6e7e9c8a3cd60ab2f7561e
Exterminator08 <rafael.chalabyan@gmail.com>
2024-04-15 11:40:54 +0200

    objFixx

--- client/src/components/RPG/src/levels/phase1objects.js
+++ client/src/components/RPG/src/levels/phase1objects.js
@@ -8,27 +8,27 @@ export const phase1objects = {
   rod1: {
     x: 43,
     y: 42,
-    dialogID: 2,
+    dialogID: 8,
   },
   rod2: {
     x: 53,
     y: 49,
-    dialogID: 3,
+    dialogID: 9,
   },
   rod3: {
     x: 33,
     y: 24,
-    dialogID: 4,
+    dialogID: 10,
   },
   dialogBubble: {
     x: 26,
     y: 51,
-    dialogID: 1,
+    dialogID: 7,
   },
   npc1: {
     x: 24,
     y: 18,
-    dialogID: 6,
+    dialogID: 12,
     exit: {
       x: 416,
       y: 272,
@@ -38,7 +38,7 @@ export const phase1objects = {
   npc2: {
     x: 24,
     y: 33,
-    dialogID: 5,
+    dialogID: 11,
     exit: {
       x: 0,
       y: 0,
--- client/src/components/RPG/src/levels/phase2objects.js
+++ client/src/components/RPG/src/levels/phase2objects.js
@@ -8,27 +8,27 @@ export const phase2objects = {
   rod1: {
     x: 40, // 40
     y: 44, // 44
-    dialogID: 2,
+    dialogID: 14,
   },
   rod2: {
     x: 56, // 56
     y: 19, // 19
-    dialogID: 4,
+    dialogID: 16,
   },
   rod3: {
     x: 25, // 25
     y: 30, // 30
-    dialogID: 5,
+    dialogID: 17,
   },
   dialogBubble: {
     x: 26,
     y: 41,
-    dialogID: 1,
+    dialogID: 13,
   },
   npc1: {
     x: 46, // 46
     y: 40, // 40
-    dialogID: 3,
+    dialogID: 15,
     exit: {
       x: 0,
       y: 0,
@@ -38,7 +38,7 @@ export const phase2objects = {
   npc2: {
     x: 23, // 23
     y: 28, // 28
-    dialogID: 6,
+    dialogID: 18,
     exit: {
       x: 0,
       y: 0,
