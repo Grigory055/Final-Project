928b0e7d54e4951d9c236fe3bcec5c23ee020fc6
kvasensk <kvasenski@gmail.com>
2024-04-15 14:14:42 +0300

    added closes all

--- client/src/components/Dialogs/DialogsPhase3/DialogStartPhase3.tsx
+++ client/src/components/Dialogs/DialogsPhase3/DialogStartPhase3.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import styles from './DialogPhase3.module.css'
 import { Button } from '@mui/material'
+import { useAppDispatch } from '../../../redux/hooks'
+import { switchDialog, switchHeroWalk } from '../../../redux/RPGSlice'
 
 interface IDialog {
   text: string
@@ -10,9 +12,15 @@ const dialogStartPhase2: IDialog = {
   text: 'Вот и финишная прямая...'
 }
 
-export default function DialogStartPhase3({ handleCloseClick }) {
+export default function DialogStartPhase3() {
   
   const [dialog, setDialog] = useState<IDialog>(dialogStartPhase2)
+  const dispatch = useAppDispatch();
+
+  const handleCloseClick = () => {
+    dispatch(switchHeroWalk(true));
+    dispatch(switchDialog(false));
+  }
 
   return (
     <>
