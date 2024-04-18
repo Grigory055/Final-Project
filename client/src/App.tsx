51190f0419eb476e29c7a8e74d80a5dcaa6635b8
Grigory055 <154119543+Grigory055@users.noreply.github.com>
2024-04-18 15:16:40 +0300

    middlewar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,28 @@ import { Route, Routes } from 'react-router-dom'
 import { MainPage, StatsPage, RPGPage } from './pages'
 import Modals from './components/Modals/Modals'
 import Video from './components/video/Video'
-import ProtectedRoute from './components/router/ProtectedRoute'
+import ProtectedRouterEnd from './components/router/ProtectedRouterEnd'
+import ProjectedRouterStat from './components/router/ProjectedRouterStat'
+import ProjectedRouterLevelPhase from './components/router/ProjectedRouterLevelPhase'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='/stats' element={<StatsPage />} />
-        <Route path='/phase/:id' element={<RPGPage />} />
-        <Route path='/end' element={<Video />} />
+        
+        <Route element={<ProtectedRouterEnd />}>
+          <Route path='/end' element={<Video />} />
+        </Route>
+
+        <Route element={<ProjectedRouterStat />}>
+          <Route path='/stats' element={<StatsPage />} />
+        </Route>
+
+        <Route element={<ProjectedRouterLevelPhase />}>
+          <Route path='/phase/:id' element={<RPGPage />} />
+        </Route>
+        
       </Routes>
       <Modals />
     </>
