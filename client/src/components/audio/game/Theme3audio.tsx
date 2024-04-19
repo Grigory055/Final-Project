464a19fe828c59dfdd42f7c5dc321d75d3f1225d
kvasensk <kvasenski@gmail.com>
2024-04-19 14:43:41 +0300

    volume

--- client/src/components/audio/game/Theme3audio.tsx
+++ client/src/components/audio/game/Theme3audio.tsx
@@ -8,13 +8,10 @@ export default function Theme3audio() {
     const  voiceElement =  voiceRef.current;
 
     if ( voiceElement) {
-<<<<<<< HEAD
-      voiceElement.volume = 0.05; // Установка громкости на 50%
-      voiceElement.playbackRate = 1.0; // Установка скорости на 150%
-=======
+
       voiceElement.volume = 0.01; // Установка громкости
       voiceElement.playbackRate = 1.0; // Установка скорости на 100%
->>>>>>> 16f3fe66e63ccc8b39c3520165921fe89f232143
+
       voiceElement.addEventListener('canplay', handleCanPlay);
       voiceElement.play().catch((error) => {
         console.log('Auto-play failed:', error);
