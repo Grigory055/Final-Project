b436f0cd46a437fe268ebb2343b332394258e9c7
Exterminator08 <rafael.chalabyan@gmail.com>
2024-04-23 19:12:10 +0200

    build1

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -174,9 +174,7 @@ export function Game({ handlerDialog }:any) {
             .map((el:any) => (
               <div style={{ margin: '4px' }}>
               <div onClick={() => handleClickOpen(el.id)}>
-             
               <div style={{ border: '4px solid rgb(116, 64, 64)', padding: '8px', fontSize: '20px' }} className={el.condition === '1' ? 'oo' : el.condition === '2' ? 'aa' : el.condition === '' && 'pp'}>{el.value}</div>
-             
               </div>
               </div>
             ))}
