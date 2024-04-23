805c8c3293d2b64e6ab7ac860b1f273a27c37b3b
dmvoronkov <nukavstalika@gmail.com>
2024-04-23 22:35:56 +0500

    Merge with build and flash

--- client/src/components/RPG/RPG.tsx
+++ client/src/components/RPG/RPG.tsx
@@ -1,6 +1,6 @@
 import './style.css'
 import { useEffect, useRef } from 'react';
-import { resources, Sprite, Vector2, GameLoop, Input, gridCells, GameObject, Hero, DialogBubble, NPC, Camera, Inventory, Rod } from "./src";
+import { resources, Sprite, Vector2, GameLoop, Input, gridCells, GameObject, Hero, DialogBubble, NPC, Camera, Inventory, Rod, events } from "./src";
 import { useParams } from 'react-router-dom';
 import { phase0objects, phase0walls, phase1objects, phase1walls, phase2objects, phase2walls, phase3objects, phase3walls } from './src/levels'
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
